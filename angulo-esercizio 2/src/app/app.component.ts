f3f7b497837d68f6ffdd0c5b1dddcbda85164de7
minasi-luca <91183277+minasi-luca@users.noreply.github.com>
2024-01-17 08:19:36 +0100

    Add files via upload

---
+++ angulo-esercizio 2/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { SecondComponentComponent } from './second-component/second-component.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet,SecondComponentComponent],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'angular-gitpod';
+  text1:string="ciao";
+  text2:string="component";
+
+}
